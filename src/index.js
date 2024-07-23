38144a212139a6b73bf065d6b46b7444fb6b76ec
senay <senaykt@gmail.com>
2024-07-23 10:17:27 -0700

    Added eventlistener to form element and used type writer plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateRecipe(event) {
+  event.preventDefault();
+  new Typewriter("#recipe", {
+    strings: "new recipe",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
 
+recipeGeneratorElement = document.querySelector("#recipe-generator");
+recipeGeneratorElement.addEventListener("submit", generateRecipe);

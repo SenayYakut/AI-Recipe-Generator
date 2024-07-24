f61ec816f6b5b21adbc3afacae452e0480de1e22
senay <senaykt@gmail.com>
2024-07-24 10:56:22 -0700

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -11,8 +11,12 @@ function generateRecipe(event) {
   event.preventDefault();
   let inputButton = document.querySelector("#input-button");
   console.log(inputButton);
+  let contentButton = document.querySelector(".hidden");
+  contentButton.classList.remove("hidden");
+
+  contentButton.innerHTML = `<span class="generating">⏳Generating a recipe about ${inputButton.value}</span>`;
   let prompt = `provide a healthy recipe about ${inputButton.value} `;
-  console.log(inputButton.value);
+
   let context =
     "you are a great ai assistant , please provide me a healthy simple recipe in  5 lines, display the name of the recipe at the begining inside <strong></strong> element and give a </br> after";
   let apiKey = "7e3b4415c5b18tf300a062dfeo8d69f7";
